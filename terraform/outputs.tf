5f5b276cc91bd8405cc465a1a3b8f007947ba7ef
RedaEssawy <redaessawy81@gmail.com>
2026-03-17 19:05:43 +0200

    Modify the output file to save IPs addresses of EC2 instances in local files to use it in a script later

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,17 +1,39 @@
+# Output the public IP addresses of the EC2 instances
 output "master_node_ip" {
     value = module.master_node.ec2_public_id
     description = "The public IP address of the master node EC2 instance"
+
+  
+}
+# Save the master node IP to a local file
+resource "local_file" "master_node_ip" {
+    content  = module.master_node.ec2_public_id
+    filename = "master_node_ip.txt"
   
 }
 
+# Output the public IP address of the worker node 
 output "worker_node_ip" {
     value = module.worker_node.ec2_public_id
     description = "The public IP address of the worker node EC2 instance"
   
 }
+# Save the worker node IP to a local file
+resource "local_file" "worker_node_ip" {
+    content  = module.worker_node.ec2_public_id
+    filename = "worker_node_ip.txt"
+  
+}
 
+# Output the public IP address of the self-hosted runner EC2 instance
 output "runner_ip" {
     value = module.runner.ec2_public_id
     description = "The public IP address of the self-hosted runner EC2 instance"
+  
+}
+# Save the runner IP to a local file
+resource "local_file" "runner_ip" {
+    content  = module.runner.ec2_public_id
+    filename = "runner_ip.txt"
   
 }
